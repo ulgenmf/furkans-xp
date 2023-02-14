502e3f1a2f4665051270b1c005fe7db02cf406b6
xavier <ulgenmf@gmail.com>
2023-02-14 15:32:01 +0100

    idk

--- src/pages/start/index.jsx
+++ src/pages/start/index.jsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import images from "../../constants/images";
 import { useState, useEffect } from "react";
+import Name from "@/components/Name/Name";
 
 function index() {
 	const router = useRouter();
@@ -12,33 +13,20 @@ function index() {
 	const [isHidden, setIsHidden] = useState("");
 
 	useEffect(() => {
-		setTimeout(() => {}, setOpa("100"));
-		setTimeout(() => {
-			setIsHidden("hidden");
-		}, 3000);
 		setTimeout(() => {
 			router.push("../desktop");
-		}, 4000);
+		}, 8100);
 	}, []);
 
 	return (
-		<div className=" h-screen  overflow-clip w-screen">
-			<div className={`h-screen bg-black ${isHidden} w-screen`}>
-				<Image
-					src={images.windows}
-					alt="s"
-					className={`h-screen  transation-opacity opacity-${opa}    duration-[2s] w-screen`}
-				/>
+		<div className=" h-screen  overflow-hidden w-screen">
+			<div className="flex h-screen w-screen items-center justify-center p-6 bg-black flex-col">
+				<Name />
+				<div className="relative flex">
+					<Image src={images.loading} width={600} />
+					<div className="h-3 w-32 bg-black absolute bottom-0 right-0"></div>
+				</div>
 			</div>{" "}
-			<div
-				className={`h-screen  relative flex items-center justify-center   bg-[#4F85F1]`}
-			>
-				<div className="bg-blue-800  absolute w-screen border-b-2  top-0 h-[15%] "></div>
-
-				<Image src={images.windowsp} className=" h-[900px]  w-[50%]" />
-
-				<div className="bg-blue-800 absolute border-t-2  w-screen bottom-0 h-[15%] "></div>
-			</div>
 		</div>
 	);
 }
